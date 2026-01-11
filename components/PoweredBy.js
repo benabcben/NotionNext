20fd6945997a2352d8cc344bce45150d7414b086
bieji <63357233+benabcben@users.noreply.github.com>
2026-01-11 10:40:06 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,19 +1,7 @@
-import { siteConfig } from '@/lib/config'
-
 /**
- * 驱动版权
- * @returns
+ * 这是一个被掏空的 PoweredBy 组件
+ * 无论放在哪，它都不会显示任何东西
  */
-export default function PoweredBy(props) {
-  return (
-    <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
-      <a
-        href='https://github.com/tangly1024/NotionNext'
-        className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
-      </a>
-      .
-    </div>
-  )
+export default function PoweredBy() {
+  return null // 直接返回空，什么都不渲染
 }
